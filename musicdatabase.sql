89d2874efad109eb346166f1e99f90403fecd3bc
Erik Larson <erik.a.larson@wmich.edu>
2018-11-30 13:26:39 -0500

    Create musicdatabase.sql
    Adding Database to tuneshare. feel free to make any changes to it that you see fit

---
+++ musicdatabase.sql
@@ -0,0 +1,36 @@
+DROP DATABASE IF EXISTS `musicdatabase`;
+CREATE DATABASE IF NOT EXISTS `musicdatabase`;
+USE `musicdatabase`;
+
+#
+# Table structure for songs.
+#
+DROP TABLE IF EXISTS `Songs`;
+CREATE TABLE `Songs` (
+`song_id` INTEGER NOT NULL AUTO_INCREMENT,
+`song_name` VARCHAR(255) NOT NULL,
+`artist_name` VARCHAR(255) NOT NULL,
+`genre` VARCHAR(100) NOT NULL,
+`song_url` VARCHAR(100) NOT NULL,
+`song_img` VARCHAR(100),
+`user_id` INT(5),
+`date_added` DATETIME NOT NULL,
+`times_played` int(5),
+PRIMARY KEY (song_id),
+FOREIGN KEY (user_id) REFERENCES Users(user_id) 
+);
+
+#
+# Table stucture for users.
+#
+DROP TABLE IF EXISTS `Users`;
+CREATE TABLE `Users` (
+`user_id` INTEGER NOT NULL AUTO_INCREMENT,
+`username` VARCHAR(255) NOT NULL UNIQUE,
+`first_name` VARCHAR(100) NOT NULL,
+`last_name` VARCHAR(100) NOT NULL,
+`email` VARCHAR(100) NOT NULL UNIQUE,
+`password` VARCHAR(100) NOT NULL,
+`user_img` VARCHAR(100),
+PRIMARY KEY (user_id)
+);
